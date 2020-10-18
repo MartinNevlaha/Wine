0ebf0c04f18fa934ca6e16ec1bf948a6494dc85d
MartinNevlaha <martin.nevlaha@gmail.com>
2020-10-18 12:30:17 +0200

    add table head to wine groups ediet

--- client/src/components/AdminMenu/WineGroups/EditWineGroups/EditWineGroups.jsx
+++ client/src/components/AdminMenu/WineGroups/EditWineGroups/EditWineGroups.jsx
@@ -1,9 +1,82 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSort } from '@fortawesome/free-solid-svg-icons';
 
-const EditWineGroup = props => (
-    <div>
-        EditWineGroups
-    </div>
-)
+import classes from './EditWineGroups.module.css';
+import ElementWrapper from '../../../../hoc/ElementWrapper/ElementWrapper';
+
+const EditWineGroup = props => {
+    
+    return (
+        <ElementWrapper wrapperType='FullWidthWrapper'>
+            <table className={classes.ShowWines}>
+                <thead>
+                    <tr>
+                        <td>
+                            <span>Číslo vína</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("id")}/>
+                        </td>
+                        <td>
+                            <span>Súťažná kategória</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("competitiveCategory")}/>
+                        </td>
+                        <td>
+                            <span>Názov vína</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("name")}/>
+                        </td>
+                        <td>
+                            <span>Klasifikácia vína</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("clasification")}/>
+                        </td>
+                        <td>
+                            <span>Farba vína</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("color")}/>
+                        </td>
+                        <td>
+                            <span>Charakter vína</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("character")}/>
+                        </td>
+                        <td>
+                            <span>Výrobca vína</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("producer")}/>
+                        </td>
+                        <td>
+                            <span>Ročník vína</span>
+                            <FontAwesomeIcon 
+                            icon={faSort}
+                            cursor="pointer" 
+                            onClick={() => this.props.sortWine("vintage")}/>
+                        </td>
+                        <td>Pridaj do skupiny</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+        </table>
+        </ElementWrapper>
+    )
+}
 
 export default EditWineGroup;
