b1e24487cab3dd49a6e00f9b6f39a397aeb9a8bc
MartinNevlaha <martin.nevlaha@gmail.com>
2020-10-25 12:18:25 +0100

    fix same icon in result menu

--- client/src/components/AdminMenu/WineResults/ResultsMenuComponents/ResultsMenuComponent.jsx
+++ client/src/components/AdminMenu/WineResults/ResultsMenuComponents/ResultsMenuComponent.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faWineGlass, faUsers, faUser } from '@fortawesome/free-solid-svg-icons';
 
 import ElementWrapper from '../../../../hoc/ElementWrapper/ElementWrapper';
 import Button from '../../../UI/Button/Button';
 import classes from './ResultsMenuComponent.module.css';
 
-const ResultsMenuComponent = props => (
-    <ElementWrapper wrapperType="SmallWrapper">
-        <h4>{props.name}</h4>
-        <FontAwesomeIcon icon={faBars} size="5x"/>
-        <div className={classes.BtnContainer}>
-            <Button 
-            index={props.index}
-            clicked={props.clicked}>Zobraz</Button>
-        </div>
-    </ElementWrapper>
-);
+const ResultsMenuComponent = props => {
+    let icon;
+    if (props.index === 0) {
+        icon = faWineGlass
+    } else if (props.index === 1) {
+        icon = faUsers
+    } else {
+        icon = faUser
+    }
+    return (
+        <ElementWrapper wrapperType="SmallWrapper">
+            <h4>{props.name}</h4>
+            <FontAwesomeIcon icon={icon} size="5x"/>
+            <div className={classes.BtnContainer}>
+                <Button 
+                index={props.index}
+                clicked={props.clicked}>Zobraz</Button>
+            </div>
+        </ElementWrapper>
+    );
+}
+
 
 
 export default ResultsMenuComponent;
