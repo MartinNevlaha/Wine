71685f86275198fef6c779c888370cbb9acabe59
MartinNevlaha <martin.nevlaha@gmail.com>
2020-11-08 18:27:03 +0100

    add enviroment variables

--- client/src/components/ErrorBoundary/ErrorBoundary.jsx
+++ client/src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -24,7 +24,7 @@ class ErrorBoundary extends Component {
         if (error) {
             return (
             <div>
-                <h1>Niečo sa pokazilo</h1>
+                <h1>Niečo sa pokazilo !!!</h1>
                 <h2>{error.toString()}</h2>
                 <pre>{info.componentStack}</pre>
                 <button onClick={this.resetHandler}>Skús znova</button>               
