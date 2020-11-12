8cdacab93c61474c07d574c15c2255750392c650
MartinNevlaha <martin.nevlaha@gmail.com>
2020-11-12 17:42:16 +0100

    responsive back el

--- client/src/components/UI/Back/Back.jsx
+++ client/src/components/UI/Back/Back.jsx
@@ -5,8 +5,22 @@ import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 
 import classes from './Back.module.css';
 
-
 class Back extends Component {
+    state = {
+        windowWidth: window.innerWidth,
+    }
+    componentDidMount() {
+        window.addEventListener('resize', this.resizeHandler);
+    }
+    componentWillUnmount() {
+        window.addEventListener('resize', this.resizeHandler);
+        this.setState = (state,callback)=>{ //fix warn react issue
+            return;
+        };
+    }
+    resizeHandler = (e) => {
+        this.setState({windowWidth: window.innerWidth});
+    }
     // add push to history back
     goBack = () => {
         this.props.history.goBack();
@@ -25,9 +39,8 @@ class Back extends Component {
             >
                 <FontAwesomeIcon 
                     icon={faAngleLeft}
-                    size='3x'
+                    size={this.state.windowWidth > 1300 ? '3x' : '2x'}
                 />
-                <p>Späť</p>
             </div>
         );
     }
