1816d393f641ac7997923b880e9e374858a09e06
MartinNevlaha <martin.nevlaha@gmail.com>
2020-12-04 16:40:02 +0100

    fix issue with helper

--- client/src/shared/utility.js
+++ client/src/shared/utility.js
@@ -1,42 +1,43 @@
 export const updateObj = (oldObject, updatedProperties) => {
-    return {
-        ...oldObject,
-        ...updatedProperties
-    }
+  return {
+    ...oldObject,
+    ...updatedProperties,
+  };
 };
 
 export const searchIdetificator = (e, tableHeadName, tableHeadId) => {
-    const chossenHeader = e.target.value;
-    let chossenHeadId = '';
-    if (chossenHeader === tableHeadName[0]) {
-        chossenHeadId = tableHeadId[0];
-    } else if (chossenHeader === tableHeadName[1]) {
-        chossenHeadId = tableHeadId[1] 
-    } else if (chossenHeader === tableHeadName[2]) {
-        chossenHeadId = tableHeadId[2] 
-    } else if (chossenHeader === tableHeadName[3]) {
-        chossenHeadId = tableHeadId[3] 
-    } else if (chossenHeader === tableHeadName[4]) {
-        chossenHeadId = tableHeadId[4] 
-    } else if (chossenHeader === tableHeadName[5]) {
-        chossenHeadId = tableHeadId[5] 
-    } else if (chossenHeader === tableHeadName[6]) {
-        chossenHeadId = tableHeadId[6] 
-    } else if (chossenHeader === tableHeadName[7]) {
-        chossenHeadId = tableHeadId[7] 
-    }
-    return [chossenHeader, chossenHeadId]
+  const chossenHeader = e.target.value;
+  let chossenHeadId = "";
+  if (chossenHeader === tableHeadName[0]) {
+    chossenHeadId = tableHeadId[0];
+  } else if (chossenHeader === tableHeadName[1]) {
+    chossenHeadId = tableHeadId[1];
+  } else if (chossenHeader === tableHeadName[2]) {
+    chossenHeadId = tableHeadId[2];
+  } else if (chossenHeader === tableHeadName[3]) {
+    chossenHeadId = tableHeadId[3];
+  } else if (chossenHeader === tableHeadName[4]) {
+    chossenHeadId = tableHeadId[4];
+  } else if (chossenHeader === tableHeadName[5]) {
+    chossenHeadId = tableHeadId[5];
+  } else if (chossenHeader === tableHeadName[6]) {
+    chossenHeadId = tableHeadId[6];
+  } else if (chossenHeader === tableHeadName[7]) {
+    chossenHeadId = tableHeadId[7];
+  }
+  return [chossenHeader, chossenHeadId];
 };
 
 export const searchFilter = (oldList, filteredBy, filterValue) => {
-    let filteredList = null;
-        filteredList = oldList.filter(val => 
-            val[filteredBy].toString().toLowerCase().includes(filterValue.toLowerCase()))
-    return filteredList;
+  let filteredList = null;
+  filteredList = oldList.filter((val) =>
+    val[filteredBy].toString().toLowerCase().includes(filterValue.toLowerCase())
+  );
+  return filteredList;
 };
 
 export const isTrueCheck = (array, checkParam) => {
-    const EmptyCheck = (val) => val[checkParam];
-    let disabled = array.every(EmptyCheck);
-    return !disabled;
-}
+  const EmptyCheck = (val) => val[checkParam];
+  let disabled = array.every(EmptyCheck);
+  return !disabled;
+};
