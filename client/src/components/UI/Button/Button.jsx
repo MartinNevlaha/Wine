e328e8d91a7e6bd2aeb4e1194c866555c522564a
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-02 14:49:16 +0100

    fix render issue

--- client/src/components/UI/Button/Button.jsx
+++ client/src/components/UI/Button/Button.jsx
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import classes from './Button.module.css';
+import classes from "./Button.module.css";
 
 class btn extends Component {
-    render() {
-        let currentClass = [];
-        if (this.props.active) {
-            currentClass.push('Active')
-        }
-        return (
-            <button
-            disabled={this.props.disabled}
-            onClick={() => this.props.clicked(this.props.index, this.props.children, this.props.btnType)}
-            className={!this.props.disabled ? [classes.Btn, classes[currentClass]].join(' '): [classes.Btn, classes.Disabled].join(' ') }
-            >{this.props.children}</button>
-        );
+  componentDidUpdate() {
+    console.log("update");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      nextProps.disabled !== this.props.disabled ||
+      nextProps.active !== this.props.active
+    );
+  }
+
+  render() {
+    let currentClass = [];
+    if (this.props.active) {
+      currentClass.push("Active");
     }
-} 
+    return (
+      <button
+        disabled={this.props.disabled}
+        onClick={() =>
+          this.props.clicked(
+            this.props.index,
+            this.props.children,
+            this.props.btnType
+          )
+        }
+        className={
+          !this.props.disabled
+            ? [classes.Btn, classes[currentClass]].join(" ")
+            : [classes.Btn, classes.Disabled].join(" ")
+        }
+      >
+        {this.props.children}
+      </button>
+    );
+  }
+}
 
 export default btn;
