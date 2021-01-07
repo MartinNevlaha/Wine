cdd756c3bea6f7b43b119ee8678503c6a14d17c4
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-07 18:05:30 +0100

    fix socket.io connection issue

--- server/socket.js
+++ server/socket.js
@@ -2,7 +2,7 @@ let io;
 
 module.exports = {
   init: (httpServer) => {
-    io = require("socket.io")(httpServer, {origins: `http://${process.env.HOST}`}); 
+    io = require("socket.io")(httpServer); 
     return io;
   },
   getIO: () => {
