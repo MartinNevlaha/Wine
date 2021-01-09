20e062668f6e91be8fea63951193c4f593f47d91
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-09 15:29:49 +0100

    fix scroll issue on components

--- client/src/components/AdminMenu/WineResults/ResultsByCategory/ResultsByCategory.jsx
+++ client/src/components/AdminMenu/WineResults/ResultsByCategory/ResultsByCategory.jsx
@@ -1,145 +1,182 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import * as action from '../../../../store/actions/index';
-import ElementWrapper from '../../../../hoc/ElementWrapper/ElementWrapper';
-import Back from '../../../UI/Back/Back';
-import Button from '../../../UI/Button/Button';
-import classes from './ResultsCategory.module.css';
-import CategoryTable from './CategoryTable/CategoryTable';
-import Popup from '../../../UI/Popup/Popup';
-import DownloadFile from '../../DownloadFile/DownloadFile';
-import { isTrueCheck } from '../../../../shared/utility';
-import Modal from '../../../UI/Modal/Modal';
-import Spinner from '../../../UI/Spinner/Spinner';
+import * as action from "../../../../store/actions/index";
+import ElementWrapper from "../../../../hoc/ElementWrapper/ElementWrapper";
+import Back from "../../../UI/Back/Back";
+import Button from "../../../UI/Button/Button";
+import classes from "./ResultsCategory.module.css";
+import CategoryTable from "./CategoryTable/CategoryTable";
+import Popup from "../../../UI/Popup/Popup";
+import DownloadFile from "../../DownloadFile/DownloadFile";
+import { isTrueCheck } from "../../../../shared/utility";
+import Modal from "../../../UI/Modal/Modal";
+import Spinner from "../../../UI/Spinner/Spinner";
 
 class ResultsByCategory extends Component {
-    state = {
-        tableHeadNames: ['Poradie', 'Číslo vína', 'Názov vína', 'Klasifikácia vína', 'Výrobca vína', 'Ročník vína', 'Degustačná skupina','Bodové hodnotenie', 'Kategória vína', 'Kompletnosť hodnotenia' ],
-        selectedCategory: null,
-        isModalShow: false,
-        error: null
-    }
-    componentDidMount() {
-        this.props.onFetchCompetitiveCategory(this.props.token)
-    }
+  state = {
+    tableHeadNames: [
+      "Poradie",
+      "Číslo vína",
+      "Názov vína",
+      "Klasifikácia vína",
+      "Výrobca vína",
+      "Ročník vína",
+      "Degustačná skupina",
+      "Bodové hodnotenie",
+      "Kategória vína",
+      "Kompletnosť hodnotenia",
+    ],
+    selectedCategory: null,
+    isModalShow: false,
+    error: null,
+  };
+  componentDidMount() {
+    this.props.onFetchCompetitiveCategory(this.props.token);
+  }
 
-    getCategoryHandler =(e) => {
-        let index = e.target.selectedIndex;
-        let el = e.target.childNodes[index];
-        let _id = el.getAttribute('id');
-        this.setState({selectedCategory: _id})
-    }
-    clickHandler= (_id) => {
-        this.props.history.push(`results-by-wineId/${_id}`)
-    }
-    fetchResultsByComCategory = () => {
-        let category;
-        !this.state.selectedCategory ? category = this.props.competitiveCategory[0]._id : category = this.state.selectedCategory;
-        this.props.onFetchResultsByComCategory(category, this.props.token)
-    }
-    writeFinalResultsHandler = () => {
-        const catId = this.state.selectedCategory || this.props.competitiveCategory[0]._id;
-        const index = this.props.competitiveCategory.findIndex(i => i._id === catId);
-        this.props.onWriteFinalResults(catId, index, this.props.token)
-        this.setState({isModalShow: false})
-    }
-    toggleModalHandler = () => {
-        this.setState({
-            isModalShow: !this.state.isModalShow,
-        })
-    }
-    errorDownloadHandler = (err) => {
-        this.setState({
-            error: err
-        })
-        setTimeout(()=>{
-            this.setState({error: null})
-        }, 2500)
-    }
+  getCategoryHandler = (e) => {
+    let index = e.target.selectedIndex;
+    let el = e.target.childNodes[index];
+    let _id = el.getAttribute("id");
+    this.setState({ selectedCategory: _id });
+  };
+  clickHandler = (_id) => {
+    this.props.history.push(`results-by-wineId/${_id}`);
+  };
+  fetchResultsByComCategory = () => {
+    let category;
+    !this.state.selectedCategory
+      ? (category = this.props.competitiveCategory[0]._id)
+      : (category = this.state.selectedCategory);
+    this.props.onFetchResultsByComCategory(category, this.props.token);
+  };
+  writeFinalResultsHandler = () => {
+    const catId =
+      this.state.selectedCategory || this.props.competitiveCategory[0]._id;
+    const index = this.props.competitiveCategory.findIndex(
+      (i) => i._id === catId
+    );
+    this.props.onWriteFinalResults(catId, index, this.props.token);
+    this.setState({ isModalShow: false });
+  };
+  toggleModalHandler = () => {
+    this.setState({
+      isModalShow: !this.state.isModalShow,
+    });
+  };
+  errorDownloadHandler = (err) => {
+    this.setState({
+      error: err,
+    });
+    setTimeout(() => {
+      this.setState({ error: null });
+    }, 2500);
+  };
 
-    render() {
-        let message;
-        if (this.props.error) {
-            message = this.props.error.message
-        } else if (this.props.isSucces) {
-            message = this.props.succesMessage
-        } else if (this.state.error) {
-            message = this.state.error.message
-        }
-        return (
-            <ElementWrapper wrapperType="ElementWrapper">
-                <Back />
-                <Modal
-                show={this.state.isModalShow} 
-                closeModal={this.toggleModalHandler}>
-                    <p>Naozaj chcete zapísať výsledky do databázy ?</p>
-                    <Button clicked={this.toggleModalHandler}>Nie</Button>
-                    <Button clicked={this.writeFinalResultsHandler}>Ano</Button>
-                </Modal>
-                <ElementWrapper wrapperType="FullWidthWrapper">
-                    <h4>Výsledky podľa súťažnej categórie vín</h4>
-                    <p>Stav výsledkov: {this.props.isFinalResultWrite? 'Finálne výsledky - zapísané': 'Priebežné výsledky - nezapísané'}</p>
-                    <div className={classes.HeaderCategoryChoose}>
-                        <label >Súťažná kategória vína</label>
-                        <select
-                        onChange={this.getCategoryHandler}>
-                        {this.props.competitiveCategory.map(cat => (
-                        <option 
-                            key={cat._id}
-                            id={cat._id}>
-                            {cat.categoryName}
-                        </option>
-                    ))}
-                        </select>
-                        <Button clicked={this.fetchResultsByComCategory}>Zobraz</Button>
-                        <DownloadFile
-                        errorDownload={this.errorDownloadHandler}
-                        endPoint="final-results-export-by-cat"
-                        token={this.props.token}
-                        fileName="result_by_cat.xlsx"
-                        isComplete={isTrueCheck(this.props.competitiveCategory, 'isFinalResultWrite')}
-                        >Stiahnuť kompletné výsledky</DownloadFile>
-                        <Button
-                        clicked={this.toggleModalHandler}
-                        disabled={isTrueCheck(this.props.results, 'isComplete')}
-                        >Zapísať výsledky skupiny</Button>
-                    </div>
-                    {this.props.loading ? <Spinner /> : 
-                    <CategoryTable 
-                    results={this.props.results}
-                    clickHandler={this.clickHandler}
-                    tableHead={this.state.tableHeadNames}
-                    />
-                    }
-                </ElementWrapper>
-                <Popup 
-                isSucces={this.props.isSucces}
-                succesMessage={this.props.succesMessage}
-                show={this.props.error || this.state.error || this.props.isSucces}
-                message={message}/>
-            </ElementWrapper>
-        )
+  render() {
+    let message;
+    if (this.props.error) {
+      message = this.props.error.message;
+    } else if (this.props.isSucces) {
+      message = this.props.succesMessage;
+    } else if (this.state.error) {
+      message = this.state.error.message;
     }
+    return (
+      <ElementWrapper wrapperType="ElementWrapper">
+        <Back />
+        <Modal
+          show={this.state.isModalShow}
+          closeModal={this.toggleModalHandler}
+        >
+          <p>Naozaj chcete zapísať výsledky do databázy ?</p>
+          <Button clicked={this.toggleModalHandler}>Nie</Button>
+          <Button clicked={this.writeFinalResultsHandler}>Ano</Button>
+        </Modal>
+        <ElementWrapper wrapperType="FullWidthWrapper">
+          <h4>Výsledky podľa súťažnej categórie vín</h4>
+          <p>
+            Stav výsledkov:{" "}
+            {this.props.isFinalResultWrite
+              ? "Finálne výsledky - zapísané"
+              : "Priebežné výsledky - nezapísané"}
+          </p>
+          <div className={classes.HeaderCategoryChoose}>
+            <label>Súťažná kategória vína</label>
+            <select onChange={this.getCategoryHandler}>
+              {this.props.competitiveCategory.map((cat) => (
+                <option key={cat._id} id={cat._id}>
+                  {cat.categoryName}
+                </option>
+              ))}
+            </select>
+            <Button clicked={this.fetchResultsByComCategory}>Zobraz</Button>
+            <DownloadFile
+              errorDownload={this.errorDownloadHandler}
+              endPoint="final-results-export-by-cat"
+              token={this.props.token}
+              fileName="result_by_cat.xlsx"
+              isComplete={isTrueCheck(
+                this.props.competitiveCategory,
+                "isFinalResultWrite"
+              )}
+            >
+              Stiahnuť kompletné výsledky
+            </DownloadFile>
+            <Button
+              clicked={this.toggleModalHandler}
+              disabled={isTrueCheck(this.props.results, "isComplete")}
+            >
+              Zapísať výsledky skupiny
+            </Button>
+          </div>
+          {this.props.loading ? (
+            <Spinner />
+          ) : (
+            <div className={classes.ResultsByCategoty_table}>
+              <CategoryTable
+                results={this.props.results}
+                clickHandler={this.clickHandler}
+                tableHead={this.state.tableHeadNames}
+              />
+            </div>
+          )}
+        </ElementWrapper>
+        <Popup
+          isSucces={this.props.isSucces}
+          succesMessage={this.props.succesMessage}
+          show={this.props.error || this.state.error || this.props.isSucces}
+          message={message}
+        />
+      </ElementWrapper>
+    );
+  }
 }
-const mapStateToProps = state => {
-    return {
-        token: state.adminAuth.token,
-        competitiveCategory: state.finalResults.competitiveCategory,
-        results: state.finalResults.results,
-        error: state.finalResults.error,
-        isFinalResultWrite: state.finalResults.isFinalResultWrite,
-        loading: state.finalResults.loading,
-        isSucces: state.finalResults.isSucces,
-        succesMessage: state.finalResults.message
-    }
-}
-const mapDispatchToProps = dispatch => {
-    return {
-        onFetchCompetitiveCategory: (token) => dispatch(action.fetchCompetitiveCategory(token)),
-        onFetchResultsByComCategory: (categoryId, token) => dispatch(action.fetchWineResultsByComCategory(categoryId, token)),
-        onWriteFinalResults: (catId, index, token) => dispatch(action.writeFinalResults(catId, index, token))
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps) (withRouter(ResultsByCategory));
+const mapStateToProps = (state) => {
+  return {
+    token: state.adminAuth.token,
+    competitiveCategory: state.finalResults.competitiveCategory,
+    results: state.finalResults.results,
+    error: state.finalResults.error,
+    isFinalResultWrite: state.finalResults.isFinalResultWrite,
+    loading: state.finalResults.loading,
+    isSucces: state.finalResults.isSucces,
+    succesMessage: state.finalResults.message,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchCompetitiveCategory: (token) =>
+      dispatch(action.fetchCompetitiveCategory(token)),
+    onFetchResultsByComCategory: (categoryId, token) =>
+      dispatch(action.fetchWineResultsByComCategory(categoryId, token)),
+    onWriteFinalResults: (catId, index, token) =>
+      dispatch(action.writeFinalResults(catId, index, token)),
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(ResultsByCategory));
--- client/src/components/AdminMenu/WineResults/ResultsByDegGroup/ResultsByDegGroup.jsx
+++ client/src/components/AdminMenu/WineResults/ResultsByDegGroup/ResultsByDegGroup.jsx
@@ -1,128 +1,147 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import * as action from '../../../../store/actions/index';
-import ElementWrapper from '../../../../hoc/ElementWrapper/ElementWrapper';
-import Back from '../../../UI/Back/Back';
-import classes from './ResultsByDegGroup.module.css';
-import Button from '../../../UI/Button/Button';
-import ResultsTable from '../ResultsTable/ResultsTable';
-import Modal from '../../../UI/Modal/Modal';
-import WineGlass from '../../../UI/WineGlass/WineGlass';
-import ResumeTable from '../../../Rating/ResumeResults/ResumeTable/ResumeTable';
-import Popup from '../../../UI/Popup/Popup';
-import Spinner from '../../../UI/Spinner/Spinner';
+import * as action from "../../../../store/actions/index";
+import ElementWrapper from "../../../../hoc/ElementWrapper/ElementWrapper";
+import Back from "../../../UI/Back/Back";
+import classes from "./ResultsByDegGroup.module.css";
+import Button from "../../../UI/Button/Button";
+import ResultsTable from "../ResultsTable/ResultsTable";
+import Modal from "../../../UI/Modal/Modal";
+import WineGlass from "../../../UI/WineGlass/WineGlass";
+import ResumeTable from "../../../Rating/ResumeResults/ResumeTable/ResumeTable";
+import Popup from "../../../UI/Popup/Popup";
+import Spinner from "../../../UI/Spinner/Spinner";
 
 class ResultsByDeGroup extends Component {
-    state = {
-        tableHeads: ['Číslo vína', 'Názov vína', 'Klasifikácia vína', 'Výrobca vína','Ročník vína' ,'Číslo degustátora','Meno degustátora', "Víno eliminované", 'Kategoria vína', 'Bodové hodnotenie' ],
-        isModalOpen: false,
-        wineId: null,
-        selectedGroup: null
+  state = {
+    tableHeads: [
+      "Číslo vína",
+      "Názov vína",
+      "Klasifikácia vína",
+      "Výrobca vína",
+      "Ročník vína",
+      "Číslo degustátora",
+      "Meno degustátora",
+      "Víno eliminované",
+      "Kategoria vína",
+      "Bodové hodnotenie",
+    ],
+    isModalOpen: false,
+    wineId: null,
+    selectedGroup: null,
+  };
+  componentDidMount() {
+    this.props.onFetchDegGroupsRes(this.props.token);
+  }
+  getGroupHandler = (e) => {
+    let index = e.target.selectedIndex;
+    let el = e.target.childNodes[index];
+    let _id = el.getAttribute("id");
+    this.setState({ selectedGroup: _id });
+  };
+  onClickHandler = (_id) => {
+    this.setState({
+      isModalOpen: true,
+      wineId: _id,
+    });
+  };
+  fetchResultByGroupHandler = () => {
+    let selectedGroup;
+    !this.state.selectedGroup
+      ? (selectedGroup = this.props.degGroups[0]._id)
+      : (selectedGroup = this.state.selectedGroup);
+    this.props.onFetchResultsByGroup(selectedGroup, this.props.token);
+  };
+  closeModalHandler = () => {
+    this.setState({ isModalOpen: false });
+  };
+  render() {
+    let detailedData = {};
+    if (this.state.wineId) {
+      detailedData = this.props.results.filter(
+        (result) => result._id === this.state.wineId
+      );
     }
-    componentDidMount() {
-        this.props.onFetchDegGroupsRes(this.props.token)
-    }
-    getGroupHandler = (e) => {
-        let index = e.target.selectedIndex;
-        let el = e.target.childNodes[index];
-        let _id = el.getAttribute('id');
-        this.setState({selectedGroup: _id})
-    }
-    onClickHandler = (_id) => {
-        this.setState({
-            isModalOpen: true,
-            wineId: _id
-        })
-    }
-    fetchResultByGroupHandler = () => {
-        let selectedGroup;
-        !this.state.selectedGroup ? selectedGroup = this.props.degGroups[0]._id : selectedGroup = this.state.selectedGroup;
-        this.props.onFetchResultsByGroup(selectedGroup, this.props.token)
-    }
-    closeModalHandler = () => {
-        this.setState({isModalOpen: false})
-    }
-    render() {
-        let detailedData = {}
-        if (this.state.wineId) {
-            detailedData = this.props.results.filter(result => 
-                result._id === this.state.wineId
-            )
-        }
-        return (
-            <ElementWrapper wrapperType="ElementWrapper">
-                <Modal show={this.state.isModalOpen}
-                closeModal={this.closeModalHandler}>
-                    <h4>Detailné zobrazenie hodnotenia vína</h4>
-                    <WineGlass />
-                    {this.state.wineId && <ResumeTable data={detailedData[0]} />}
-                    <Button clicked={this.closeModalHandler}>Ok</Button>
-                </Modal>
-                <Back />
-                <ElementWrapper wrapperType="FullWidthWrapper">
-                    <h4>Výsledky vo vybranej degustačnej skupine</h4>
-                    <div className={classes.HeaderGroupChoose}>
-                            <label>Degustačná skupina</label>
-                            <select
-                            onChange={this.getGroupHandler}>
-                            {this.props.degGroups.map(cat => (
-                            <option 
-                                key={cat._id}
-                                id={cat._id}>
-                                {cat.groupName}
-                            </option>
-                        ))}
-                            </select>
-                        <Button clicked={this.fetchResultByGroupHandler}>Zobraz</Button>
-                    </div>
-                    {this.props.loading ? <Spinner /> : 
-                    <ResultsTable
-                        tableHeads={this.state.tableHeads}>
-                        {this.props.results.map((result, index) => {
-                        return (
-                        <tr key ={result._id}
-                        onClick={() => this.onClickHandler(result._id)}>
-                            <td>{result.wineDbId.id}</td>
-                            <td>{result.wineDbId.name}</td>
-                            <td>
-                                {`${result.wineDbId.color} 
+    return (
+      <ElementWrapper wrapperType="ElementWrapper">
+        <Modal
+          show={this.state.isModalOpen}
+          closeModal={this.closeModalHandler}
+        >
+          <h4>Detailné zobrazenie hodnotenia vína</h4>
+          <WineGlass />
+          {this.state.wineId && <ResumeTable data={detailedData[0]} />}
+          <Button clicked={this.closeModalHandler}>Ok</Button>
+        </Modal>
+        <Back />
+        <ElementWrapper wrapperType="FullWidthWrapper">
+          <h4>Výsledky vo vybranej degustačnej skupine</h4>
+          <div className={classes.HeaderGroupChoose}>
+            <label>Degustačná skupina</label>
+            <select onChange={this.getGroupHandler}>
+              {this.props.degGroups.map((cat) => (
+                <option key={cat._id} id={cat._id}>
+                  {cat.groupName}
+                </option>
+              ))}
+            </select>
+            <Button clicked={this.fetchResultByGroupHandler}>Zobraz</Button>
+          </div>
+          {this.props.loading ? (
+            <Spinner />
+          ) : (
+            <div className={classes.ResultsByGroup_table}>
+              <ResultsTable tableHeads={this.state.tableHeads}>
+                {this.props.results.map((result, index) => {
+                  return (
+                    <tr
+                      key={result._id}
+                      onClick={() => this.onClickHandler(result._id)}
+                    >
+                      <td>{result.wineDbId.id}</td>
+                      <td>{result.wineDbId.name}</td>
+                      <td>
+                        {`${result.wineDbId.color} 
                                 ${result.wineDbId.character} 
                                 ${result.wineDbId.clasification}`}
-                            </td>
-                            <td>{result.wineDbId.producer}</td>
-                            <td>{result.wineDbId.vintage}</td>
-                            <td>{result.degId.id}</td>
-                            <td>{`${result.degId.name} ${result.degId.surname}`}</td>
-                            <td>{result.eliminated ? "Áno" : 'Nie'}</td>
-                            <td>{result.wineCategory}</td>
-                            <td>{result.totalSum}</td>
-                        </tr>
-                        )
-                    })}
-                    </ResultsTable>
-                    }
-                </ElementWrapper>
-                <Popup 
-                show={this.props.error}
-                message={this.props.error && this.props.error.message}/>
-            </ElementWrapper>
-        )
-    }
+                      </td>
+                      <td>{result.wineDbId.producer}</td>
+                      <td>{result.wineDbId.vintage}</td>
+                      <td>{result.degId.id}</td>
+                      <td>{`${result.degId.name} ${result.degId.surname}`}</td>
+                      <td>{result.eliminated ? "Áno" : "Nie"}</td>
+                      <td>{result.wineCategory}</td>
+                      <td>{result.totalSum}</td>
+                    </tr>
+                  );
+                })}
+              </ResultsTable>
+            </div>
+          )}
+        </ElementWrapper>
+        <Popup
+          show={this.props.error}
+          message={this.props.error && this.props.error.message}
+        />
+      </ElementWrapper>
+    );
+  }
 }
-const mapStateToProps = state => {
-    return {
-        token: state.adminAuth.token,
-        degGroups: state.finalResults.degGroups,
-        results: state.finalResults.resultsByGroup,
-        error: state.finalResults.error,
-        loading: state.finalResults.loading
-    }
-}
-const mapDispatchToProps = dispatch => {
-    return {
-        onFetchDegGroupsRes: (token) => dispatch(action.fetchDegGroupsRes(token)),
-        onFetchResultsByGroup: (groupId, token) => dispatch(action.fetchResultsByGroup(groupId, token))
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps) (ResultsByDeGroup);
+const mapStateToProps = (state) => {
+  return {
+    token: state.adminAuth.token,
+    degGroups: state.finalResults.degGroups,
+    results: state.finalResults.resultsByGroup,
+    error: state.finalResults.error,
+    loading: state.finalResults.loading,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchDegGroupsRes: (token) => dispatch(action.fetchDegGroupsRes(token)),
+    onFetchResultsByGroup: (groupId, token) =>
+      dispatch(action.fetchResultsByGroup(groupId, token)),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ResultsByDeGroup);
