e28bce49c1561750bc75c834fcac7ed152448105
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-11 17:30:03 +0100

    add https option to qr code

--- server/controllers/adminSettings.js
+++ server/controllers/adminSettings.js
@@ -1,57 +1,67 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
-const Setting = require('../models/settings');
+const Setting = require("../models/settings");
 
 exports.setSettings = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error('Niektoré z poskytnutých dát sú nesprávne')
-        error.statusCode = 422;
-        return next(error);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Niektoré z poskytnutých dát sú nesprávne");
+    error.statusCode = 422;
+    return next(error);
+  }
+  const {
+    isValuesEliminated,
+    isDegustationOpen,
+    degustationName,
+    competitionChairman,
+  } = req.body;
+  try {
+    const setting = await Setting.find();
+    if (!setting) {
+      const error = new Error("Nemôžem zmeniť nastavenia degustácie");
+      error.statusCode = 404;
+      return next(error);
     }
-    const {isValuesEliminated, isDegustationOpen, degustationName, competitionChairman} = req.body;
-    try {
-        const setting = await Setting.find(); 
-        if (!setting) {
-            const error = new Error('Nemôžem zmeniť nastavenia degustácie');
-            error.statusCode = 404;
-            return next(error);
-        }
-        setting[0].isValuesEliminated = isValuesEliminated;
-        setting[0].isDegustationOpen = isDegustationOpen;
-        setting[0].degustationName = degustationName;
-        setting[0].competitionChairman = competitionChairman;
-        const updatedSetting = await setting[0].save();
-        res.status(200).json({
-            message: 'nastavenia boli uspešne zmenené',
-            setting: updatedSetting
-        })
-    } catch (error) {
-        if(!error.statusCode) {
-            error.statusCode = 500;
-        }
-        next(error);
+    setting[0].isValuesEliminated = isValuesEliminated;
+    setting[0].isDegustationOpen = isDegustationOpen;
+    setting[0].degustationName = degustationName;
+    setting[0].competitionChairman = competitionChairman;
+    const updatedSetting = await setting[0].save();
+    res.status(200).json({
+      message: "nastavenia boli uspešne zmenené",
+      setting: updatedSetting,
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
     }
-}
+    next(error);
+  }
+};
 
 exports.getSettings = async (req, res, next) => {
-    const host = process.env.HOST
-    try {
-        const settings = await Setting.find();
-        if (!settings) {
-            const error = new Error("Nemôžem načítať nastavenia");
-            error.statusCode = 404;
-            return next(error);
-        }
-        res.status(200).json({
-            message: 'Nastavenia načítané',
-            settings: settings[0],
-            host: 'http://' + host
-        })
-    } catch (error) {
-        if(!error.statusCode) {
-            error.statusCode = 500;
-        }
-        next(error);
+  const host = process.env.HOST;
+  const ssl = process.env.SSL;
+  let hostName = `http://${host}`;
+  if (ssl) {
+    hostName = `https://${host}`;
+  }
+  try {
+    const settings = await Setting.find();
+    if (!settings) {
+      const error = new Error("Nemôžem načítať nastavenia");
+      error.statusCode = 404;
+      return next(error);
     }
-}
+    res.status(200).json({
+      message: "Nastavenia načítané",
+      settings: settings[0],
+      host: hostName,
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
