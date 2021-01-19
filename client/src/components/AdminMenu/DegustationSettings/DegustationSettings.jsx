455fd5a4c15e85e3b2f93459367874c6868aa2aa
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-19 20:11:13 +0100

    added togle function showInfo

--- client/src/components/AdminMenu/DegustationSettings/DegustationSettings.jsx
+++ client/src/components/AdminMenu/DegustationSettings/DegustationSettings.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCogs, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCogs, faInfoCircle, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 import ElementWrapper from "../../../hoc/ElementWrapper/ElementWrapper";
 import Button from "../../UI/Button/Button";
@@ -8,17 +8,22 @@ import classes from "./DegustationSettings.module.css";
 import CardOverlay from "../../UI/CardOverlay/CardOverlay";
 
 class DegustationSettings extends Component {
-    state = {
-        isInfoShow: false
-    }
+  state = {
+    isInfoShow: false,
+  };
+  infoShowHandler = () => {
+    this.setState({ isInfoShow: !this.state.isInfoShow });
+  };
   render() {
     return (
       <ElementWrapper wrapperType="SmallWrapper">
-        <CardOverlay>125</CardOverlay>
+        <CardOverlay isShow={this.state.isInfoShow}>125</CardOverlay>
         <FontAwesomeIcon
-          icon={faInfoCircle}
+          icon={this.state.isInfoShow ? faTimes: faInfoCircle}
           size={"1x"}
           className={classes.InfoIcon}
+          onClick={this.infoShowHandler}
+          cursor="pointer"
         />
         <h4 className={classes.CardHeader}>Základné nastavenia degustácie</h4>
         <FontAwesomeIcon
--- client/src/components/UI/CardOverlay/CardOverlay.jsx
+++ client/src/components/UI/CardOverlay/CardOverlay.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
 import classes from "./CardOverlay.module.css";
 
-const CardOverlay = props => {
-
+const CardOverlay = (props) => {
   return (
-    <div className={classes.CardOverlay}>
+    <div
+      className={
+        !props.isShow
+          ? classes.CardOverlay
+          : [classes.CardOverlay, classes.Show].join(" ")
+      }
+    >
       {props.children}
     </div>
-  )
-}
+  );
+};
 
 export default CardOverlay;
