ef14fb74a8e489afba103208e30801d71ea64225
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-19 19:52:29 +0100

    rework to statefull component

--- client/src/components/AdminMenu/DegustationSettings/DegustationSettings.jsx
+++ client/src/components/AdminMenu/DegustationSettings/DegustationSettings.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCogs, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
@@ -7,19 +7,30 @@ import Button from "../../UI/Button/Button";
 import classes from "./DegustationSettings.module.css";
 import CardOverlay from "../../UI/CardOverlay/CardOverlay";
 
-const DegustationSettings = (props) => (
-  <ElementWrapper wrapperType="SmallWrapper">
-    <CardOverlay>125</CardOverlay>
-    <FontAwesomeIcon icon={faInfoCircle} size={"1x"} className={classes.InfoIcon} />
-    <h4 className={classes.CardHeader}>Základné nastavenia degustácie</h4>
-    <FontAwesomeIcon
-      icon={faCogs}
-      size={props.windowWidth <= 1300 ? "3x" : "5x"}
-    />
-    <div className={classes.BtnContainer}>
-      <Button clicked={props.clicked}>Uprav nastavenia</Button>
-    </div>
-  </ElementWrapper>
-);
+class DegustationSettings extends Component {
+    state = {
+        isInfoShow: false
+    }
+  render() {
+    return (
+      <ElementWrapper wrapperType="SmallWrapper">
+        <CardOverlay>125</CardOverlay>
+        <FontAwesomeIcon
+          icon={faInfoCircle}
+          size={"1x"}
+          className={classes.InfoIcon}
+        />
+        <h4 className={classes.CardHeader}>Základné nastavenia degustácie</h4>
+        <FontAwesomeIcon
+          icon={faCogs}
+          size={this.props.windowWidth <= 1300 ? "3x" : "5x"}
+        />
+        <div className={classes.BtnContainer}>
+          <Button clicked={this.props.clicked}>Uprav nastavenia</Button>
+        </div>
+      </ElementWrapper>
+    );
+  }
+}
 
 export default DegustationSettings;
