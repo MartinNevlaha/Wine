a1f329198625fe1795a504965660ed5c4253e39d
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-19 20:25:44 +0100

    finish infoCard on winelist

--- client/src/components/AdminMenu/WineList/WineList.jsx
+++ client/src/components/AdminMenu/WineList/WineList.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWineGlass, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWineGlass,
+  faInfoCircle,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 
-import ElementWrapper from '../../../hoc/ElementWrapper/ElementWrapper';
-import Button from '../../UI/Button/Button';
-import classes from './WineList.module.css';
+import ElementWrapper from "../../../hoc/ElementWrapper/ElementWrapper";
+import Button from "../../UI/Button/Button";
+import classes from "./WineList.module.css";
+import CardOverlay from "../../UI/CardOverlay/CardOverlay";
 
-const wineList = (props) => (
-    <ElementWrapper wrapperType="SmallWrapper">
+class WineList extends Component {
+  state = {
+    isInfoShow: false,
+  };
+  infoShowHandler = () => {
+    this.setState({ isInfoShow: !this.state.isInfoShow });
+  };
+
+  render() {
+    return (
+      <ElementWrapper wrapperType="SmallWrapper">
+        <CardOverlay isShow={this.state.isInfoShow}>
+          Pridanie vín do degustácie, ich editácia, import pomocou csv suborov,
+          vymazanie databázy vín.
+        </CardOverlay>
         <h4 className={classes.CardHeader}>Zoznam vín</h4>
-        <FontAwesomeIcon icon={faInfoCircle} size={"1x"} className={classes.InfoIcon} />
-        <FontAwesomeIcon icon={faWineGlass} size={props.windowWidth <=1300 ? '3x' : '5x'} />
+        <FontAwesomeIcon
+          icon={this.state.isInfoShow ? faTimes : faInfoCircle}
+          size={"1x"}
+          className={classes.InfoIcon}
+          onClick={this.infoShowHandler}
+          cursor="pointer"
+        />
+        <FontAwesomeIcon
+          icon={faWineGlass}
+          size={this.props.windowWidth <= 1300 ? "3x" : "5x"}
+        />
         <div className={classes.BtnContainer}>
-            {!props.isDegustationOpen && 
-            <Button clicked={props.clicked}>Uprav vína</Button>}
-            <Button clicked={props.clicked}>Zobraz vína</Button>
+          {!this.props.isDegustationOpen && (
+            <Button clicked={this.props.clicked}>Uprav vína</Button>
+          )}
+          <Button clicked={this.props.clicked}>Zobraz vína</Button>
         </div>
-    </ElementWrapper>
-);
+      </ElementWrapper>
+    );
+  }
+}
 
-export default wineList;
+export default WineList;
