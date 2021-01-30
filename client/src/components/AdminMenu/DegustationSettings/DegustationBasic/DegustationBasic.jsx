deba79b737162f02a233fe04c90065105b9bff8b
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-30 13:24:36 +0100

    restyled inputs on basic degustation settings

--- client/src/components/AdminMenu/DegustationSettings/DegustationBasic/DegustationBasic.jsx
+++ client/src/components/AdminMenu/DegustationSettings/DegustationBasic/DegustationBasic.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import classes from '../RatingSetting/RatingSetting.module.css';
+import classes from "../RatingSetting/RatingSetting.module.css";
+import styles from "./DegustationBasic.module.css";
 
-const DegustationBasic = props => (
-    <React.Fragment>
-        <div className={classes.RatingSetting}>
-            <h4>Hlavné názvy</h4>
-            <p>Názov degustácie: {props.actualDegName}</p>
-            <label>Nazov degustácie</label>
-            <input type="text"
-            onChange={e => props.getValueHandler(e, "degustationName")}/>
-            <p>Predseda degustácie: {props.actualChairman}</p>
-            <label>Predseda degustácie</label>
-            <input type="text"
-            onChange={e => props.getValueHandler(e, 'competitionChairman')}/>
-        </div>
-    </React.Fragment>
-)
+const DegustationBasic = (props) => (
+  <React.Fragment>
+    <div className={classes.RatingSetting}>
+      <h4>Hlavné názvy</h4>
+      <p>Názov degustácie: {props.actualDegName}</p>
+      <input
+        type="text"
+        placeholder="Názov degustácie"
+        className={styles.input}
+        onChange={(e) => props.getValueHandler(e, "degustationName")}
+      />
+      <p>Predseda degustácie: {props.actualChairman}</p>
+      <input
+        type="text"
+        placeholder="Predseda degustácie"
+        className={styles.input}
+        onChange={(e) => props.getValueHandler(e, "competitionChairman")}
+      />
+    </div>
+  </React.Fragment>
+);
 
 export default DegustationBasic;
