ec49aaaa01e30ae54e5d26ff5ee8207a8b39bb0c
MartinNevlaha <martin.nevlaha@gmail.com>
2021-02-20 12:23:12 +0100

    fix error on invalid token

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -1,68 +1,88 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const ADMIN_JWT_SECRET = process.env.ADMIN_JWT_SECRET;
 const DEGUSTATOR_JWT_SECRET = process.env.DEGUSTATOR_JWT_SECRET;
 
 const isAdminAuth = (req, res, next) => {
-    if (req.method === "OPTIONS") {
-        return next();
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      const error = new Error("Autentifikácia zlyhala");
+      error.statusCode = 401;
+      return next(error);
     }
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        if (!token) {
-            const error = new Error("Autentifikácia zlyhala");
-            error.statusCode = 401;
-            return next(error);
-        }
-        const decodedToken = jwt.verify(token, ADMIN_JWT_SECRET);
-        if (decodedToken.role !== 'admin') {
-            const error = new Error("Neoprávený prístup, vyžaduje sa prístup ADMINA");
-            error.statusCode = 401;
-            return next(error);
-        }
-        req.userData = {
-            adminId: decodedToken.adminId,
-            role: decodedToken.role,
-        }
-        next();
-    } catch (err) {
-        const error = new Error("Autentifikácia zlyhala");
+    const decodedToken = jwt.verify(token, ADMIN_JWT_SECRET, (err, admin) => {
+      if (err) {
+        const error = new Error("Token nie je valídny");
         error.statusCode = 401;
         return next(error);
+      }
+      return admin;
+    });
+    if (decodedToken.role !== "admin") {
+      const error = new Error("Neoprávený prístup, vyžaduje sa prístup ADMINA");
+      error.statusCode = 401;
+      return next(error);
     }
+    req.userData = {
+      adminId: decodedToken.adminId,
+      role: decodedToken.role,
+    };
+    next();
+  } catch (err) {
+    const error = new Error("Autentifikácia zlyhala");
+    error.statusCode = 401;
+    return next(error);
+  }
 };
 const isDegustatorAuth = (req, res, next) => {
-    if (req.method === "OPTIONS") {
-        return next();
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      const error = new Error("Autentifikácia zlyhala");
+      error.statusCode = 401;
+      return next(error);
     }
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        if (!token) {
-            const error = new Error('Autentifikácia zlyhala');
-            error.statusCode = 401;
-            return next(error);
+    const decodedToken = jwt.verify(
+      token,
+      DEGUSTATOR_JWT_SECRET,
+      (err, degustator) => {
+        if (err) {
+          const error = new Error("Token nie je valídny");
+          error.statusCode = 401;
+          return next(error);
         }
-        const decodedToken = jwt.verify(token, DEGUSTATOR_JWT_SECRET);
-        if (decodedToken.role != 'degustator') {
-            const error = new Error("Neoprávený prístup, vyžaduje sa prístup Degustátora");
-            error.statusCode = 401;
-            return next(error);
-        }
-        req.userData = {
-            degId: decodedToken.degId,
-            role: decodedToken.role,
-            groupId: decodedToken.groupId,
-            group: decodedToken.group
-        }
-        next();
-    } catch (err) {
-        const error = new Error("Autentifikácia zlyhala");
-        error.statusCode = 401;
-        return next(error);
+        return degustator;
+      }
+    );
+    if (decodedToken.role != "degustator") {
+      const error = new Error(
+        "Neoprávený prístup, vyžaduje sa prístup Degustátora"
+      );
+      error.statusCode = 401;
+      return next(error);
     }
+    req.userData = {
+      degId: decodedToken.degId,
+      role: decodedToken.role,
+      groupId: decodedToken.groupId,
+      group: decodedToken.group,
+    };
+    next();
+  } catch (err) {
+    const error = new Error("Autentifikácia zlyhala");
+    error.statusCode = 401;
+    return next(error);
+  }
 };
 
 module.exports = {
-    isAdminAuth,
-    isDegustatorAuth
+  isAdminAuth,
+  isDegustatorAuth,
 };
